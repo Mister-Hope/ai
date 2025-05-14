98edb9e2397a4eacce6a9bdb648e4861a3ec1ee1
Mr.Hope <mister-hope@outlook.com>
2025-05-14 18:03:13 +0800

    feat: update

--- docker/open-webui-pipelines.sh
+++ docker/open-webui-pipelines.sh
@@ -2,6 +2,6 @@ docker run -d \
   --name pipelines \
   --restart always \
   --add-host=host.docker.internal:host-gateway \
-  -p 127.0.0.1:9099:9099 \
+  -p 9099:9099 \
   -v pipelines:/app/pipelines \
   ghcr.io/open-webui/pipelines:main
